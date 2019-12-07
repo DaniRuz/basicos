8207fb5da67f05ac422300d303e201769c512999
Dani <danimarinte@gmail.com>
2019-12-07 19:12:20 +0100

    implementacion metodo cadenas
    Signed-off-by: Dani <danimarinte@gmail.com>

--- src/basico_daniel/Main.java
+++ src/basico_daniel/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		
+		cadenas();
 	}
-
+	
+	public static void cadenas() {
+		String texto;
+		texto = "Hola mundo";
+		System.out.println(texto);
+	}
 }
