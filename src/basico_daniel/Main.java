d015812f18b360b441ad384f1ebf7c1b3a3f02d0
Dani <danimarinte@gmail.com>
2019-12-07 19:01:34 +0100

    Implementacion del metodo numericos.
    Signed-off-by: Dani <danimarinte@gmail.com>

--- src/basico_daniel/Main.java
+++ src/basico_daniel/Main.java
@@ -4,7 +4,45 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		numericos();
 		
 	}
-
+	
+	public static void numericos() {
+		
+		
+		//Declaración de variables
+		
+		byte elMasPeque;
+	    short chiquitin;
+	    int entero;
+	    long duplicaEntero;
+	    float grande;
+	    double doble;
+	    
+	    //Asignar valores
+	    
+	    elMasPeque = 8;
+	    chiquitin = 20;
+	    entero = 100;
+	    duplicaEntero = 1000;
+	    grande = 34.45f;
+	    doble = 23.35295;
+	    
+	    //Mostrar variables
+	    
+	    System.out.println("Valor byte");
+	    System.out.println(elMasPeque);
+	    System.out.println("valor short");
+	    System.out.println(chiquitin);
+	    System.out.println("Valor entero");
+	    System.out.println(entero);
+	    System.out.println("Valor long");
+	    System.out.println(duplicaEntero);
+	    System.out.println("valor float");
+	    System.out.println(grande);
+	    System.out.println("valor double");
+	    System.out.println(doble);
+	    
+	    }
 }
